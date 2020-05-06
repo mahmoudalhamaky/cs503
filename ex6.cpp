032752ec4bb29bc1f002efcdfd6c070ff03a2577
Mahmoud alhamaky <mahmoud.alhamaky@gmail.com>
2020-05-07 00:26:24 +0200

    diagnosing character, number and letter with '#' will be ignored, and an empty message aborts the commit.

---
+++ ex6.cpp
@@ -0,0 +1,28 @@
+// mahmoud asim salim mahgoup alhamaky
+// cs503
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{	int num;
+	char a = a;
+	char b=b; char c=c; char d=d; char e=e; char f=f; char g=g;char h=h;char i=i;char j=j;char k=k;char l=l;char m=m;char n=n;char o=o;char p=p;char q=q;char r=r;char s=s;char t=t;char u=u;char v=v;char w=w;char x=x;char y=y;char z=z;
+	
+	
+    cout<<"\nenter your input then hit enter : \n";
+    cin>>num;
+	
+	if((num==a)||(num==b)||(num==c)||(num==d)||(num==e)||(num==f)||(num==g)||(num==h)||(num==i)||(num==j))
+    { cout<<"\nyour input is a letter  ";}
+	else if ((num==k)||(num==l)||(num==m)||(num==n)||(num==o)||(num==p)||(num==q)||(num==r)||(num==s)||(num==t))
+   	{ cout<<"\nyour input is a letter  ";}
+	else if ((num==u)||(num==v)||(num==w)||(num==x)||(num==y)||(num==z))
+   	{ cout<<"\nyour input is a letter  ";}
+	else if ((num==1)||(num==2)||(num==3)||(num==4)||(num==5)||(num==6)||(num==7)||(num==8)||(num==9)||(num==0))
+   	{ cout<<"\nyour input is a number  ";}
+   	else if(true)
+	      	{cout<<"/nyour input is a character";
+	   }
+    return 0;
+}
+
